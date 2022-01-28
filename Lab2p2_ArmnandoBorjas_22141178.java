e3c6b8bd6fff983448ea14e3566bc343b6ac5026
Ajborjas <98615109+Ajborjas@users.noreply.github.com>
2022-01-28 15:59:22 -0600

    Add files via upload

--- Lab2p2_ArmnandoBorjas_22141178.java
+++ Lab2p2_ArmnandoBorjas_22141178.java
@@ -1,14 +1,37 @@
-/*
+        /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Programacion2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
 /**
  *
- * @author choch
+ * @author Armando Borjas
  */
 public class Lab2p2_ArmnandoBorjas_22141178 {
+    public static ArrayList<Animales> lista = new ArrayList();
+    public static Animales animal1 = new Animales();
+    public static Animales animal2 = new Animales();
+    public static Animales animal3 = new Animales();
+    public static Animales logueado = new Animales();
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        animal1 = new Animales("Cobra", "Ophiophagus hannah", "Africa", "ratones", "escamas", "salvaje", 9 );
+        animal2 = new Animales("Tiburon", "Selachimorpha", "oceano", "peces", "colmillos_afilados", "salvaje", 20 );
+        animal3 = new Animales("Lobo", "Canis lupus", "Artico", "ciervos", "aullidos", "salvaje", 16 );
+        lista.add(animal1);
+        lista.add(animal2);
+        lista.add(animal3);
+        do{
+            opciones( menu() );
+        }while(true);
+    }
     
-}
+    
+    
