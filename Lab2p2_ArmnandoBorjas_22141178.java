bfa40198210407408e3b68c3cc7284cc7ad21e37
Ajborjas <98615109+Ajborjas@users.noreply.github.com>
2022-01-28 14:14:00 -0600

    Add files via upload

---
+++ Lab2p2_ArmnandoBorjas_22141178.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Programacion2;
+
+/**
+ *
+ * @author choch
+ */
+public class Lab2p2_ArmnandoBorjas_22141178 {
+    
+}
